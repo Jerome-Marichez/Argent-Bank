3107c0753ee78bfec5dd5240a0c4a71ae80e8d80
Jérôme MARICHEZ <jeromemarichez@ik.me>
2023-03-24 11:06:02 +0100

    Fix React Error
    Fix Packages and path React Error.

---
+++ src/pages/SignIn.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import AuthForm from "../components/AuthForm/AuthForm";
+
+/**
+ * 
+ * @returns A Page SignIn which wrap HTML Elements + Components
+ */
+export default function SignIn(): JSX.Element {
+
+	return (
+		<main className="main bg-dark">
+			<section id="sign-in" className="sign-in">
+				<AuthForm />
+			</section>
+		</main>
+	);
+	
+}
---
+++ src/pages/User.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import AccTransaction from "../components/AccTransaction/AccTransaction";
+import EditUser from "../components/EditUser/EditUser";
+
+import { useSelector, useDispatch } from 'react-redux';
+import { logout, updateUser } from '../redux/userSlice';
+import type { rootState } from '../redux/store';
+/**
+ * 
+ * @returns A Page User which fetch profile and load HTML Elements + Components 
+ */
+export default function User(): JSX.Element {
+
+	const dispatch = useDispatch();
+	const token: string = useSelector((state: rootState) => state.user.token);
+
+	const apiPath: string = process.env.REACT_APP_PROFILE_POST_API_URL ?? "http://localhost:3001/v1/api/user/profile";
+
+
+	const fetchUser = async () => {
+		try {
+			const response: Response = await fetch(apiPath, {
+				headers: {
+					'Accept': 'application/json',
+					'Content-Type': 'application/json',
+					'Authorization': `Bearer ${token}`,
+				},
+				method: "POST",
+			});
+
+			const responseUser: { status: number, body: object } = await response.json();
+			if (responseUser.status !== 200 && !responseUser.body) { dispatch(logout()); }
+			dispatch(updateUser(responseUser.body));
+		}
+		
+		catch {
+			dispatch(logout());
+		}
+	};
+
+	fetchUser(); 
+
+	
+	return (
+		<main className="main bg-dark">
+			<h2 className="sr-only">Accounts</h2>
+			<EditUser />
+			<AccTransaction title={"Argent Bank Checking (x8349)"} amount={"2,082.79"} />
+			<AccTransaction title={"Argent Bank Savings (x6712)"} amount={"10,928.42"} />
+			<AccTransaction title={"Argent Bank Credit Card (x8349)"} amount={"184.30"} />
+		</main>
+	);
+
+}
+
